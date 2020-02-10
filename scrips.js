aaf99296ed7e6905123ef7d310d7c816244e9169
Kimia Darden <kimiadarden@gmail.com>
2020-02-10 09:01:37 -0500

    adding the ajax

--- scrips.js
+++ scrips.js
@@ -5,4 +5,43 @@ const apiKey = "&appid=66569c356f36c97349e4385fd588495f";
 
 let date = new Date();
 
-const queryUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+$("#serchingInput").keypress(function(event) { 
+	// keycode===13 is the enter key 
+	if (event.keyCode === 13) { 
+		event.preventDefault();
+		$("#serchingBt").click(); 
+	} 
+});
+
+
+$("#serchingBt").on("click", function() {
+
+    $('#fiveDayForcast').addClass('show');
+  
+    // get the value of the input from user
+    city = $("#serchingInput").val();
+    
+    // clear input box
+    $("#serchingInput").val("");  
+  
+    // URL
+    const mainQuery = "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+  
+    $.ajax({
+      url: mainQuery,
+      method: "GET"
+    })
+    .then(function (response){
+        //converting the tempreture
+      let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+  
+    
+      })
+    });
+  
+  
+
+
+
+
+
